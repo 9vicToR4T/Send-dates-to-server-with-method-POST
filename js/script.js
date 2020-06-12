84813f7b75499fab737f0b388e6a5f377ea949ba
VV <victor@gmail.com>
2020-06-12 19:11:52 +0300

    Another method for send data from server with method POST, using and second parametrs from fetch

--- js/script.js
+++ js/script.js
@@ -13,54 +13,61 @@ window.addEventListener("DOMContentLoaded", () => {
         formData.forEach((value, key) =>{//apoi din el vom scoate totul sub formatul JSON
             obj[key] = value;
         });
-    //deja formam noul obiect in format JSON
-    let json = JSON.stringify(obj);
+   
 
     //dupa ce se creeaza DOM SE cheama aceasta functie
-    const request = new XMLHttpRequest();
-    request.open("POST", "http://localhost:3000/people"); //ADRESA am luato din json-ser) ver
+    // const request = new XMLHttpRequest();
+    // request.open("POST", "http://localhost:3000/people"); //ADRESA am luato din json-ser) ver
 
-    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    request.send(json); //in cazul POST indica ce vrem sa timitem. variabila mea json
-    request.addEventListener("load", function () {
-      if (request.status == 200) {
-        //atunci cind totul e ok
-        //response este o proprietate a obiectului XMLHttpRequest
-        let data = JSON.parse(request.response); //in response avem raspunsul de la server
-        console.log(data); //la acest moment daca totul este bine noi am primit datele de pe server
-        //createCards(data);
+    // request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    // request.send(json); //in cazul POST indica ce vrem sa timitem. variabila mea json
+    // request.addEventListener("load", function () {
+    //   if (request.status == 200) {
+    //     //atunci cind totul e ok
+    //     //response este o proprietate a obiectului XMLHttpRequest
+    //     let data = JSON.parse(request.response); //in response avem raspunsul de la server
+    //     console.log(data); //la acest moment daca totul este bine noi am primit datele de pe server
+    //     //createCards(data);
 
-      } else {
-        console.error("Something wrong");
-        //uneori poate sa ne dea erorare chiar daca obiectul a fost trimis . Trebuie asta sa o verificam
-      }
-    });
+    //   } else {
+    //     console.error("Something wrong");
+    //     //uneori poate sa ne dea erorare chiar daca obiectul a fost trimis . Trebuie asta sa o verificam
+    //   }
+    // });
 
-    // getResources("http://localhost:3000/people") //cererea spre server. ne intoarce un Promes
-    //   //.then((data) => data.json()) //primim, daca totul e bine, datele venite de la server.Aceasta nu ne intoarce un json obiect, ci un alt Promes
-    //   .then((data) => createCards(data)) //aici deja putem prelucra datele
-    //   .catch((err) => console.error(err));
+    getResources("http://localhost:3000/people", obj) //cererea spre server. ne intoarce un Promes
+                                               //obj e obiectul pe care il vom transforma in JSON
+      .then((data) => createCards(data)) //aici deja putem prelucra datele
+      .catch((err) => console.error(err));
 
     //this.remove(); //stergem butonul dupa click
   }
 
-//   async function getResources(url) {
-//     //pentru a scapa de codul care se repeta
-//     let res = await fetch(`${url}`); //folosim async await pentru a astepta datele de pe server
-
-//     if (!res.ok) {
-//       //in fetch avem proprietatea ok, care ne spune rezultatul cererii
-//       throw new Error(`Could not fetch ${url}, 
-//             status: ${res.status}`); //e bine sa cerem status deoarece fetch default nu arata care e problema
-//     }
-
-//     return await res.json(); //in loc de  .then( data => data.json())
-//   }
-
-  //obrabotcikul va avea loc pe forma cind va avea loc submit
+ //obrabotcikul va avea loc pe forma cind va avea loc submit
  form.addEventListener("submit", (e) => req(e), { once: true });
     //punem func req intro func arrow pentru ai transmite event care va putea amina reincarcarea paginii
   // al treile argument options { }. ii spunem sa lucreze doar o data
+  
+  async function getResources(url, data) {//data e datele pe care le vom trimite pe server
+    //pentru a scapa de codul care se repeta
+    let res = await fetch(`${url}`, { //folosim async await pentru a astepta datele de pe server
+                          method: "POST",
+                          headers: {//oglavlenie
+                              "Content-type": "application/json"
+                          },
+                          body: JSON.stringify(data) //ceea ce vom trimite pe server in format JSON
+    });
+
+    if (!res.ok) {
+      //in fetch avem proprietatea ok, care ne spune rezultatul cererii
+      throw new Error(`Could not fetch ${url}, 
+            status: ${res.status}`); //e bine sa cerem status deoarece fetch default nu arata care e problema
+    }
+
+    return await res.json(); //in loc de  .then( data => data.json())
+  }
+
+ 
 
   function createCards(response) {
     response.forEach((item) => {
